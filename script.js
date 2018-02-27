e7440cc65df9ef5642a1c459893a83e84bc52805
ivashmelev <36031399+ivashmelev@users.noreply.github.com>
2018-02-27 17:07:32 +0300

    1

---
+++ script.js
@@ -0,0 +1,18 @@
+page = document.querySelector("div.container-fluid");
+tr = document.createElement("tr");
+
+
+
+    for(var i = 0; i<10; i++){
+        td = document.createElement("td");
+        // page.appendChild(td);
+        // tr.appendChild(td);
+        page.appendChild(td);
+    }
+    
+console.log(page)
+    
+
+
+
+// console.log(page);
