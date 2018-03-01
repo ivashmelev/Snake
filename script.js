397b5347cfddaf0610d239ee35e9a4444e06984b
ivashmelev <36031399+ivashmelev@users.noreply.github.com>
2018-03-01 16:53:34 +0300

    4

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ y = 250;
 snake.style.left=x+"px";
 snake.style.top=y+"px";
 dist="";
-speed=5;
+speed=1;
 start=Date.now();
 width=10;
 height=10;
 snake.style.width=width+"px";
 snake.style.height=height +"px";
+bodysnake=[{x:250, y:250}];
 
 var timer = setInterval(function() { // вычислить сколько времени прошло с начала анимации
     var timePassed = Date.now() - start;
@@ -18,7 +19,7 @@ var timer = setInterval(function() { // вычислить сколько вре
   
   moveSnake(timePassed);     // рисует состояние анимации, соответствующее времени timePassed
 
-}, 10);
+}, 20);
 
 
 function moveSnake(e, timePassed){
@@ -36,24 +37,19 @@ function moveSnake(e, timePassed){
     switch(dist){
         case "right": 
             var timePassed = Date.now() - start;
-            x+=speed;
-            width=30;
-            height=10;
-            snake.style.left=x+"px";
-            snake.style.top=y+"px";
-            snake.style.width=width+"px";
-            snake.style.height=height +"px";
+            // x+=speed;
+            bodysnake.x+=speed;
+            console.log(bodysnake.x);
+            // snake.style.left=x+"px";
+            // snake.style.top=y+"px";
+
         break;
 
         case "up":
             var timePassed = Date.now() - start;
             y-=speed;
-            width=10;
-            height=30;
             snake.style.left=x+"px";
             snake.style.top=y+"px";
-            snake.style.width=width+"px";
-            snake.style.height=height +"px";
         break;
 
         case "left":
