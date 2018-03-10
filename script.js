85cf7bccc65448dc34cc39f99b705c2adaec0160
ivashmelev <shmelevivan20@gmail.com>
2018-03-11 00:36:05 +0300

    10

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-field = document.querySelector("div.field");  
+field = document.querySelector("div.field");
 segment ="";
 snakeBody=[];
-var weight=4;
+var weight=5;
 dist="";
 x=250;
 y=250;
@@ -11,19 +11,16 @@ speed=1000;
         segment = document.createElement("div");
         segment.className="segment-snake";
         segment.innerHTML = i;
-        snakeBody.push({segment:segment, x:x-=10, y:y});
+        snakeBody.push({segment:segment, x:x, y:y});
         field.appendChild(snakeBody[i].segment);
+        ll = snakeBody[i].segment.style.top= snakeBody[i].y+"px";
+        snakeBody[i].segment.style.left= snakeBody[i].x+"px";
     }
 
-    for (var i=0; i<weight; i++){
-        ll = snakeBody[i].segment.style.top= snakeBody[i].y+"px";
-        snakeBody[i].segment.style.left= snakeBody[i].x+"px";           
-    }
-    
 
     line = snakeBody.length-1; // Последний элемент массива
     count = snakeBody.length-line;
-    
+
 
     console.log(line);
 
@@ -36,13 +33,13 @@ speed=1000;
 
     console.log("x: "+xHead+" | "+"y: "+yHead);
 
-    
+
 
 start = Date.now();
 var timer = setInterval(function() { // вычислить сколько времени прошло с начала анимации
     var timePassed = Date.now() - start;
 
-  
+
   moveSnake(timePassed);     // рисует состояние анимации, соответствующее времени timePassed
 
 }, speed);
@@ -51,15 +48,15 @@ var timer = setInterval(function() { // вычислить сколько вре
 max=490;
 min=0;
 rabbit=null;
-        
+
     if (rabbit==null){
         randomX = Math.floor(Math.floor(Math.random()*(max-min+1)+min) / 10) * 10;
         randomY = Math.floor(Math.floor(Math.random()*(max-min+1)+min) / 10) * 10;
         rabbit = document.createElement("div");
-        rabbit.className="apple";
+        rabbit.className="rabbit";
         field.appendChild(rabbit);
         rabbit.style.top=250+"px";
-        rabbit.style.left=270+"px"; 
+        rabbit.style.left=270+"px";
         console.log(parseInt(rabbit.style.left),parseInt(rabbit.style.top));
     }
 
@@ -70,32 +67,31 @@ function moveSnake(e, timePassed,speed){
 
             randomX = Math.floor(Math.floor(Math.random()*(max-min+1)+min) / 10) * 10;
             randomY = Math.floor(Math.floor(Math.random()*(max-min+1)+min) / 10) * 10;
-        
+
+
+
 
             rabbit = document.createElement("div");
-            rabbit.className="apple";
+            rabbit.className="rabbit";
             field.appendChild(rabbit);
+
             rabbit.style.top=randomY+"px";
-            rabbit.style.left=randomX+"px"; 
+            rabbit.style.left=randomX+"px";
             weight+=1;
-            snakeBody.splice(0,0,{segment:segment, x:snakeBody[0].x+10, y:snakeBody[0].y});
-            // snakeBody.pop();
-                // field.removeChild(snakeBody[line].segment);
-                
-            
-            // segment = document.createElement("div");
-            // segment.className="segment-snake";
-            // segment.innerHTML = i;
-            // snakeBody.push({segment:segment, x:x-=10, y:y});
-            // field.appendChild(snakeBody[line].segment);
-            
-            this.speed-=900;
-            
+
+            for (var i=0; i<1; i++){
+                    segment = document.createElement("div");
+                    segment.className="segment-snake";
+                    segment.innerHTML = i;
+                    field.appendChild(segment);
+                    snakeBody[i].segment.style.top= snakeBody[i].y+"px";
+                    snakeBody[i].segment.style.left= snakeBody[i].x+"px";
+            }
             console.log(snakeBody);
             console.log(weight);
 
     }
-     
+
     switch(e.keyCode){
         case 39: dist="right";
         break;
@@ -109,12 +105,12 @@ function moveSnake(e, timePassed,speed){
 
     switch(dist){
         case "right":
-                
+
                 snakeBody.splice(0,0,{segment:segment, x:snakeBody[0].x+10, y:snakeBody[0].y});
                 snakeBody.pop();
                 field.removeChild(snakeBody[line].segment);
-                
-                for (var i=0; i<1; i++){    
+
+                for (var i=0; i<1; i++){
                         segment = document.createElement("div");
                         segment.className="segment-snake";
                         segment.innerHTML = i;
@@ -125,10 +121,10 @@ function moveSnake(e, timePassed,speed){
                 if (snakeBody[0].x==snakeBody[2].x){
                     snakeBody[0].y-=10;
                     snakeBody[0].segment.style.top=snakeBody[0].y+"px";
-                    
+
                     // console.log("alarm!");
                 }
-                
+
                 // console.log(snakeBody[0].x+" | "+snakeBody[1].x);
         break;
 
@@ -147,7 +143,7 @@ function moveSnake(e, timePassed,speed){
             if (snakeBody[0].y==snakeBody[2].y){
                 snakeBody[0].x+=10;
                 snakeBody[0].segment.style.left=snakeBody[0].x+"px";
-                    
+
                 // console.log("alarm!");
             }
         break;
@@ -167,7 +163,7 @@ function moveSnake(e, timePassed,speed){
                 if (snakeBody[0].x==snakeBody[2].x){
                     snakeBody[0].y-=10;
                     snakeBody[0].segment.style.top=snakeBody[0].y+"px";
-                    
+
                     // console.log("alarm!");
                 }
 
@@ -189,29 +185,28 @@ function moveSnake(e, timePassed,speed){
             if (snakeBody[0].y==snakeBody[2].y){
                 snakeBody[0].x+=10;
                 snakeBody[0].segment.style.left=snakeBody[0].x+"px";
-                        
+
                 // console.log("alarm!");
             }
         break;
-        
+
     }
 
-    
-}
-        
-        
 
-        console.log(snakeBody[0].x, snakeBody[0].y);
-        // } 
-        
+}
+
+
+
+        // }
+
         // if(randomX%5==0){
-        //     rabbit.style.left=randomX+"px";  
+        //     rabbit.style.left=randomX+"px";
         // }
         // if(randomY%5==0){
-        //     rabbit.style.top=randomY+"px";  
-        // }           
-        console.log(randomX); 
-        console.log(randomY);   
-            
+        //     rabbit.style.top=randomY+"px";
         // }
-addEventListener ("keydown", moveSnake)
+        console.log(randomX);
+        console.log(randomY);
+
+        // }
+addEventListener ("keydown", moveSnake);
